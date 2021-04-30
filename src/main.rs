60728dfea194ab393e97e1e23b9f9ba1372805bb
Kees van Voorthuizen <me@keesvv.nl>
2021-04-30 17:59:38 +0200

    feat: add support for chaining transformers

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,27 @@ use std::{convert::TryFrom, env::args, io::{Read, stdin}};
 use transfrm::TransformType;
 
 fn main() {
-    let args: Vec<String> = args().collect();
+    let mut args: Vec<String> = args().collect();
     let mut input = String::new();
+    let mut output;
+    let mut transfrm_types: Vec<TransformType> = Vec::new();
+
+    args.drain(0..1);
+
+    for arg in args.iter() {
+        let transfrm_type: TransformType = TransformType::try_from(
+            arg.as_str()
+        ).unwrap();
+
+        transfrm_types.push(transfrm_type);
+    }
 
     stdin().read_to_string(&mut input).unwrap();
+    output = input.clone();
 
-    let transfrm_type: TransformType = TransformType::try_from(
-        args[1].as_str()
-    ).unwrap();
-
-    print!("{}", transfrm_type.transform(&input).trim());
+    for ttype in transfrm_types.iter() {
+        output = ttype.transform(output.as_str());
+    }
+ 
+    print!("{}", output.trim());
 }
