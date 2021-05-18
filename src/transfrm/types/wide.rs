67dacb57121376492a6c930c89efa4d08ce1d211
Kees van Voorthuizen <me@keesvv.nl>
2021-05-18 22:23:44 +0200

    fix(wide): take multibyte chars into account

--- src/transfrm/types/wide.rs
+++ src/transfrm/types/wide.rs
@@ -3,10 +3,10 @@ const WIDTH: usize = 1;
 pub fn wide(s: &str) -> String {
     let mut output = String::new();
 
-    for c in s.as_bytes() {
+    for c in s.chars() {
         output.push_str(
             String::from(
-                format!("{}{}", *c as char, " ".repeat(WIDTH))
+                format!("{}{}", c, " ".repeat(WIDTH))
             ).as_str()
         )
     }
